d46730861c1079e06d3ba0c25da8efa16a8ab04f
maatini <martin.richardt@gmail.com>
2026-04-20 17:53:47 +0200

    chore: preload test support modules in test_helper to prevent parallel compilation race conditions

--- kwik_e_mart/test/test_helper.exs
+++ kwik_e_mart/test/test_helper.exs
@@ -1,1 +1,5 @@
 ExUnit.start()
+# Preload test support modules before ExUnit compiles test files in parallel.
+# Without this, parallel compilation races cause "module not loaded" errors.
+Code.ensure_loaded!(KwikEMart.DataCase)
+Code.ensure_loaded!(KwikEMartWeb.ConnCase)
